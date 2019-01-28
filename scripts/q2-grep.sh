6d82c1dd76a38d6a66e4a2dbf82179c9f9b884d8
v1254 <dtmohamed95@gmail.com>
2019-01-28 11:52:21 +0000

    fix file to match coursework

--- scripts/q2-grep.sh
+++ scripts/q2-grep.sh
@@ -1,7 +1,12 @@
 # walk through parameters and grep them if they exist in the current working directory
 while [ "$1" != "" ];
 do 
-	result=$(grep --with-filename -A 1 -B 1 "$USER" "$1") 
+	result=$(grep --with-filename -C 1 "$USER" "$1") 
 	echo "$result" && shift
+	## after shifting check if next element is empty, print dash only if it isn't.
+	if [ "$1" != "" ];
+	then
+		echo "--"
+	fi
 done
 
