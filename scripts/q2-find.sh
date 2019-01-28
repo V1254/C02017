04062394ae3fdcaf2472a94da858589daf5ed5de
v1254 <dtmohamed95@gmail.com>
2019-01-28 13:24:44 +0000

    finish question 2 part 2

---
+++ scripts/q2-find.sh
@@ -0,0 +1,25 @@
+if [ -z "$1" ];
+then
+	echo 'Error: provide a directory path '
+	exit 1
+fi
+
+echo "Plain files modified more than 72 hours ago in $1"
+for i in $(find "$1" -maxdepth 1 -type f -mmin +4320)
+do
+	echo "$i"
+done
+
+echo "Directories in sub-directories of $1 starting with \"dir\":"
+for i in $(find "$1" -type d -name "*dir*")
+do
+	echo "$i"
+done
+
+
+
+
+
+
+
+
