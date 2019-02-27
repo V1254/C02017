7648a14a49057caf296e5f2cdc6f956cdad67b22
Mohamed <dtmohamed95@gmail.com>
2019-02-27 18:49:54 +0000

    add more implementations for both classes.

--- cw2/src/CO2017/exercise2/masf1/BestFitMemManager.java
+++ cw2/src/CO2017/exercise2/masf1/BestFitMemManager.java
@@ -1,4 +1,64 @@
 package CO2017.exercise2.masf1;
 
-public class BestFitMemManager {
+/**
+ * Concrete Class to represent a MemManager using a best fit allocation policy.
+ *
+ * @author (masf1) 179029141
+ */
+public class BestFitMemManager extends MemManager {
+
+    /**
+     * Construct a BestFitMemManager instance.
+     *
+     * @param s the total size of memory to be created
+     */
+
+    public BestFitMemManager(int s) {
+        super(s);
+    }
+
+    /**
+     * Find an address space large enough for s using the best fit strategy
+     *
+     * @param s the size of memory that needs to be allocated.
+     * @return the address of a space of at least size s.
+     */
+    @Override
+    protected int findSpace(int s) {
+
+        int startAddress = -1;
+        int currentBestSize = 0;
+
+        // counter to iterate through _memory.length
+        int counter = 0;
+
+        while (counter < _memory.length) {
+
+            // use the inherited method to find the largest size of free spaces from the current position.
+            int availableSize = countFreeSpacesAt(counter);
+
+            // check availSize is enough to fit s.
+            if (availableSize >= s) {
+
+                // update the startAddress the first time an availableSpace is found for s.
+                if (startAddress < 0) {
+                    startAddress = counter;
+                }
+                // update the value of size depending on the currentAvailable size.
+                // if the availAble size is smaller than the current size and also fits S then it is considered the better size.
+                if (availableSize < currentBestSize) {
+                    currentBestSize = availableSize;
+                    startAddress = counter;
+                }
+            }
+
+            // jump to the next position after the free spaces or go the next counter.
+            counter += 1 > availableSize ? 1 : availableSize;
+        }
+
+        // returns -1 if no address is available, else it returns the address found from the loop.
+        return startAddress;
+    }
+
+
 }
--- cw2/src/CO2017/exercise2/masf1/WorstFitMemManager.java
+++ cw2/src/CO2017/exercise2/masf1/WorstFitMemManager.java
@@ -1,4 +1,66 @@
 package CO2017.exercise2.masf1;
 
-public class WorstFitMemManager {
+/**
+ * Concrete Class to represent a MemManager using a worst fit allocation policy.
+ *
+ * @author (masf1) 179029141
+ */
+public class WorstFitMemManager extends MemManager {
+
+
+    /**
+     * Construct a WorstFitMemManager instance.
+     *
+     * @param s - the total size of memory to be created
+     */
+    public WorstFitMemManager(int s) {
+        super(s);
+    }
+
+    /**
+     * Find an address space large enough for s using the worst fit strategy
+     *
+     * @param s the size of memory that needs to be allocated.
+     * @return the address of a space of at least size s.
+     */
+
+    @Override
+    protected int findSpace(int s) {
+
+//        // details about the worst fit so far.
+//        int address = -1;
+//        int currentSize = 0;
+//
+//        // counter to go through the _memory
+//        int counter = 0;
+//
+//        while (counter < _memory.length) {
+//
+//            int availableSize = countFreeSpacesAt(counter);
+//
+//            // see if space is at least enough to fit s.
+//            if (availableSize >= s) {
+//
+//                // update the address the first time some space is found that can hold s.
+//                if (address < 0) {
+//                    address = counter;
+//                }
+//
+//                // worst fit.
+//                if (currentSize < availableSize) {
+//                    address = counter;
+//                    currentSize = availableSize;
+//                }
+//
+//            }
+//
+//            counter += currentSize > 1 ? currentSize : 1;
+//        }
+
+        return 0;
+
+
+    }
+
+
 }
